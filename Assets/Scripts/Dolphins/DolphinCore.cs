f7bc08a352b2b90de8f60bdceeac3dd03888f712
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 14:36:57 +0900

    Move and rotate

--- Assets/Scripts/Dolphins/DolphinCore.cs
+++ Assets/Scripts/Dolphins/DolphinCore.cs
@@ -8,9 +8,17 @@ namespace Dolphins
         [SerializeField]
         PlayerInputEventProvider input;
 
+        [SerializeField]
+        float defaultSpeed;
+        [SerializeField]
+        float defaultRotateSpeed;
+
         public IReadOnlyReactiveProperty<bool> Fire => input.Fire;
         public IReadOnlyReactiveProperty<bool> Sonar => input.Sonar;
         public IReadOnlyReactiveProperty<Vector2> Move => input.Move;
         public IReadOnlyReactiveProperty<Vector2> Aim => input.Aim;
+
+        public float Speed => defaultSpeed;
+        public float RotateSpeed => defaultRotateSpeed;
     }
 }
