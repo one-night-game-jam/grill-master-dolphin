968b95ee3cf98fce13667f9b6e48dbd74c96000c
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 20:21:45 +0900

    Title

---
+++ Assets/Scripts/Common/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using UniRx.Async;
+using UniRx.Async.Triggers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Common
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField]
+        Button button;
+
+        [SerializeField]
+        string sceneName;
+
+        void Start()
+        {
+            ClickAndLoadSceneAsync().Forget();
+        }
+
+        async UniTaskVoid ClickAndLoadSceneAsync()
+        {
+            var cancellationToken = this.GetCancellationTokenOnDestroy();
+            await button.OnClickAsync(cancellationToken);
+            await SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
