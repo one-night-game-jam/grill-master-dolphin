862d4c499a182a9df066f19c749d47785746848a
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 17:49:18 +0900

    Grill

---
+++ Assets/Scripts/Fishes/FishCore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Fishes
+{
+    public class FishCore : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject grilledPrefab;
+
+        public void Burn()
+        {
+            Instantiate(grilledPrefab, transform.position, transform.rotation, transform.parent);
+            Destroy(gameObject);
+        }
+    }
+}
