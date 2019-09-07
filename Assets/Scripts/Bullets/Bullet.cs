d324203990ca835ef2eb470d5e020800d0e08c6e
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 15:16:48 +0900

    Fire

---
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Bullets
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField]
+        Rigidbody rb;
+        [SerializeField]
+        Transform collisionTransform;
+
+        [SerializeField]
+        float speed;
+        [SerializeField]
+        float scaleRatio;
+
+        [SerializeField]
+        float LifetimeSeconds;
+
+        void Start()
+        {
+            this.FixedUpdateAsObservable()
+                .Subscribe(_ => rb.MovePosition(rb.transform.position += rb.transform.forward * speed * Time.deltaTime))
+                .AddTo(this);
+            this.UpdateAsObservable()
+                .Subscribe(_ => collisionTransform.localScale += Vector3.one * scaleRatio * Time.deltaTime)
+                .AddTo(this);
+
+            Destroy(gameObject, LifetimeSeconds);
+        }
+    }
+}
