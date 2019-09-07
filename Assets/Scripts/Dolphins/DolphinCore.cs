65798afa0fd4a0294a38eb15c7cd68c5cf19e2dd
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 16:00:22 +0900

    Fugu makes dolphin happy

--- Assets/Scripts/Dolphins/DolphinCore.cs
+++ Assets/Scripts/Dolphins/DolphinCore.cs
@@ -13,12 +13,30 @@ namespace Dolphins
         [SerializeField]
         float defaultRotateSpeed;
 
+        [SerializeField]
+        float happySpeed;
+        [SerializeField]
+        float happyRotateSpeed;
+
+        float happyTime;
+        bool IsHappy => happyTime > 0;
+
         public IReadOnlyReactiveProperty<bool> Fire => input.Fire;
         public IReadOnlyReactiveProperty<bool> Sonar => input.Sonar;
         public IReadOnlyReactiveProperty<Vector2> Move => input.Move;
         public IReadOnlyReactiveProperty<Vector2> Aim => input.Aim;
 
-        public float Speed => defaultSpeed;
-        public float RotateSpeed => defaultRotateSpeed;
+        public float Speed => IsHappy ? happySpeed : defaultSpeed;
+        public float RotateSpeed => IsHappy ? happyRotateSpeed : defaultRotateSpeed;
+
+        public void MakeHappy(float time)
+        {
+            happyTime = Mathf.Max(0, happyTime) + time;
+        }
+
+        void Update()
+        {
+            happyTime -= Time.deltaTime;
+        }
     }
 }
---
+++ Assets/Scripts/Fishes/Fugu.cs
@@ -0,0 +1,17 @@
+﻿using Dolphins;
+using UnityEngine;
+
+namespace Fishes
+{
+    public class Fugu : MonoBehaviour
+    {
+        [SerializeField]
+        float happyTime;
+
+        void OnTriggerEnter(Collider other)
+        {
+            var dolphinCore = other.GetComponent<DolphinCore>();
+            if (dolphinCore != null) dolphinCore.MakeHappy(happyTime);
+        }
+    }
+}
