1231e871919129b9891c7294b3c74e9ac835ff63
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 23:56:22 +0900

    Fix bug

--- Assets/Scripts/Dolphins/DolphinCore.cs
+++ Assets/Scripts/Dolphins/DolphinCore.cs
@@ -30,9 +30,10 @@ namespace Dolphins
         public const float PlayTime = 60;
         public float PastTime { get; private set; }
         public float LastTime => PlayTime - PastTime;
-        public IObservable<bool> IsTimeUp => this.ObserveEveryValueChanged(x => PlayTime < x.PastTime);
+        bool IsTimeUp => PlayTime < PastTime;
+        public IObservable<bool> IsTimeUpAsObservable => this.ObserveEveryValueChanged(x => x.IsTimeUp);
 
-        public IObservable<bool> Fire => input.Fire.CombineLatest(IsTimeUp, (fire, isTimeUp) => fire && !isTimeUp);
+        public IObservable<bool> Fire => input.Fire.CombineLatest(IsTimeUpAsObservable, (fire, isTimeUp) => fire && !isTimeUp);
         public IObservable<Unit> Sonar => input.Sonar
             .Where(b => b)
             .ThrottleFirst(TimeSpan.FromSeconds(sonarCooldownTime))
@@ -46,7 +47,8 @@ namespace Dolphins
         public void MakeHappy(float time)
         {
             HappyTime = Mathf.Max(0, HappyTime) + time;
-            PastTime -= 10f;
+            if (!IsTimeUp)
+                PastTime -= 10f;
             Debug.Log($"Happy time {HappyTime}");
         }
 
