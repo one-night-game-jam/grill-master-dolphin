6b178fa7316ef3722a3e5f22340384ad6762664c
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 19:51:25 +0900

    Move fishes

---
+++ Assets/Scripts/Fishes/FishMover.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using UniRx.Async;
+using UniRx.Async.Triggers;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Fishes
+{
+    public class FishMover : MonoBehaviour
+    {
+        void Start()
+        {
+            RandomMoveTask().Forget();
+        }
+
+        async UniTaskVoid RandomMoveTask()
+        {
+            var cancellationToken = this.GetCancellationTokenOnDestroy();
+            while (true)
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(Random.value * 3f), cancellationToken: cancellationToken);
+                await LookAtRandom(cancellationToken);
+                await UniTask.Delay(TimeSpan.FromSeconds(Random.value * 2f), cancellationToken: cancellationToken);
+                await MoveForwardRandom(cancellationToken);
+            }
+        }
+
+        async UniTask LookAtRandom(CancellationToken cancellationToken)
+        {
+            var targetRotation = Quaternion.LookRotation(Random.onUnitSphere);
+            var startTime = Time.timeSinceLevelLoad;
+            var duration = Random.Range(0.5f, 2f);
+            while (true)
+            {
+                var lastTime = startTime + duration - Time.timeSinceLevelLoad;
+                if (lastTime < 0)
+                {
+                    transform.rotation = targetRotation;
+                    return;
+                }
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, (duration - lastTime) / duration);
+                await UniTask.Yield();
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+        }
+
+        async UniTask MoveForwardRandom(CancellationToken cancellationToken)
+        {
+            var targetPosition = transform.position + transform.forward * Random.Range(5f, 20f);
+            var startTime = Time.timeSinceLevelLoad;
+            var duration = Random.Range(5f, 10f);
+            while (true)
+            {
+                var lastTime = startTime + duration - Time.timeSinceLevelLoad;
+                if (lastTime < 0)
+                {
+                    transform.position = targetPosition;
+                    return;
+                }
+                transform.position = Vector3.Slerp(transform.position, targetPosition, (duration - lastTime) / duration);
+                await UniTask.Yield();
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+        }
+    }
+}
