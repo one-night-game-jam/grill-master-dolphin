68efe94a6538e327c0fe6e45f0ea247a62f4694c
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 15:29:37 +0900

    Add sonar skeleton

---
+++ Assets/Scripts/Fishes/EnvironmentObserver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Dolphins;
+using UniRx;
+using UnityEngine;
+
+namespace Fishes
+{
+    public class EnvironmentObserver : MonoBehaviour
+    {
+        [SerializeField]
+        DolphinCore dolphinCore;
+
+        void Start()
+        {
+            dolphinCore.Sonar
+                .Subscribe(_ => OnSonar())
+                .AddTo(this);
+        }
+
+        void OnSonar()
+        {
+            
+        }
+    }
+}
