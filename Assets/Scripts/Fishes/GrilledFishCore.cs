953ab3923817b8fb7245d1790a925657bd1a712b
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 18:25:20 +0900

    Get grilled fishes

--- Assets/Scripts/Fishes/GrilledFishCore.cs
+++ Assets/Scripts/Fishes/GrilledFishCore.cs
@@ -1,4 +1,6 @@
 ﻿using Dolphins;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace Fishes
@@ -7,6 +9,16 @@ namespace Fishes
     {
         [SerializeField]
         long score;
+
+        [SerializeField]
+        float speed;
+
+        public void Inject(DolphinCore dolphinCore)
+        {
+            this.UpdateAsObservable()
+                .Subscribe(_ => transform.position = Vector3.Lerp(transform.position, dolphinCore.transform.position, Time.deltaTime * speed))
+                .AddTo(this);
+        }
 
         public void Touch(DolphinCore dolphinCore)
         {
