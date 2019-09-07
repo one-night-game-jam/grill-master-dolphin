2dc112c6ffaf6a7ef32b476ed167e26fc8e49fbd
Shoma SATO <noir.neo.04@gmail.com>
2019-09-07 13:01:25 +0900

    Implement inputs provider

---
+++ Assets/Scripts/Dolphins/DolphinCore.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UnityEngine;
+
+namespace Dolphins
+{
+    public class DolphinCore : MonoBehaviour
+    {
+        [SerializeField]
+        PlayerInputEventProvider input;
+
+        public IReadOnlyReactiveProperty<bool> Fire => input.Fire;
+        public IReadOnlyReactiveProperty<bool> Sonar => input.Sonar;
+        public IReadOnlyReactiveProperty<Vector2> Move => input.Move;
+        public IReadOnlyReactiveProperty<Vector2> Aim => input.Aim;
+    }
+}
